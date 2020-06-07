d4abb1943cbc9d18dbcc2e2d5a5400383d7cdb99
Agurshageok <san.ferreiro92@gmail.com>
2020-06-07 03:37:41 -0300

    Avazando...

--- tateti.pl
+++ tateti.pl
@@ -1,22 +1,36 @@
+%%%%%%%%%%%%%
+% 1 | 2 | 3 %
+%---------- %
+% 4 | 5 | 6 %
+%---------- %
+% 7 | 8 | 9 %
+%%%%%%%%%%%%%
+
 %Definir el Tablero de Juego
-tablero=
-       [hueco(1,l),
-        hueco(2,l),
-        hueco(3,l),
-        hueco(4,l),
-        hueco(5,l),
-        hueco(6,l),
-        hueco(7,l),
-        hueco(8,l),
-        hueco(9,l),]
+hueco(1,libre).
+hueco(2,libre).
+hueco(3,libre).
+hueco(4,libre).
+hueco(5,libre).
+hueco(6,libre).
+hueco(7,libre).
+hueco(8,libre).
+hueco(9,libre).
 
 
 %Ocupar hueco
-ocupar(Pos, _) :- hueco(Pos, l). 
+ocuparPorMaquina(Pos, _) :- hueco(Pos, cruz). 
+ocuparPorJugador(Pos, _) :- hueco(Pos, circulo). 
+
+centro_libre (_) :- true.
+arriba_libre (_) :- true.
+abajo_libre (_) :- true.
+izq_libre (_) :- true.
+der_libre (_) :- true.
 
 
 %Funciones para insertar las jugadas.
-jugada(Pos,[H|Hs]) :-   ocupar(Pos, [H|Hs]),
+jugada(Pos,[H|Hs]) :-   ocuparPorJugador(Pos, [H|Hs]),
                         centro_libre([H|Hs]), 
                         arriba_libre([H|Hs]), 
                         abajo_libre([H|Hs]), 
